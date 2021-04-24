1e7359b4d9b9631cb235d5ddde616bdbadc7c6ad
arnavk1 <arnavkashyap3108@gmail.com>
2021-04-25 00:40:30 +0530

    error fixed

--- Programs/draw_contours.py
+++ Programs/draw_contours.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("python-logo.png")
+img = cv2.imread("opencv-logo.png")
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
 
